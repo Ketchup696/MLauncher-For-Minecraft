b81390236de16f9b89825c2eeb6c823e2af04be0
Sapphire <53796461+Sapphire588@users.noreply.github.com>
2026-04-25 06:54:45 +0700

    Revert "Return List"
    This reverts commit 9f2f15987a13d979721353f53519d54e08de8f30.

--- MoonLauncher/Form1.cs
+++ MoonLauncher/Form1.cs
@@ -84,8 +84,7 @@ namespace MoonLauncher
 
             if (_settings.SavedNicknames is null || _settings.SavedNicknames.Count is 0)
             {
-                _settings.SavedNicknames = new List<string>();
-                _settings.SavedNicknames.Add(defaultNickname);
+                _settings.SavedNicknames = [defaultNickname]; // use array instead of List
                 SaveSettings();
             }
 
@@ -237,7 +236,7 @@ namespace MoonLauncher
                         cmbNicknames.Text = _settings.SavedNicknames.FirstOrDefault() ?? defaultNickname;
 
                     //cmbNicknames.DataSource = null;
-                    cmbNicknames.DataSource = _settings.SavedNicknames;
+                    cmbNicknames.DataSource = _settings.SavedNicknames.ToList();
                     cmbNicknames.Text = _settings.SavedNicknames.FirstOrDefault() ?? defaultNickname;
                 }
             }
